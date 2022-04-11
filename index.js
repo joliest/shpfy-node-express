6b7597392e6712f4601811017fb5c6f12befab4f
Joliver Estampador <joliverestampador@gmail.com>
2022-04-11 17:34:33 +0800

    Adding /shopify route

--- index.js
+++ index.js
@@ -12,8 +12,41 @@ const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'write_products';
 const forwardingAddress = process.env.NGROK_ADDR;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/shopify', (req, res) => {
+    const shop = req.query.shop;
+    if (shop) {
+        /**
+         * What is nonce for?
+         *      ↳ random string that nonce will take care for us. We send it as request
+         *      once we receive the response from Shopify, it will echo the state so that we can
+         *      compare the two.
+         *           ↳ if they match - it came from shopify
+         *           ↳ if they dont match - it is invalid and returns an error
+         */
+        const state = nonce();
+        const redirectUri = `${forwardingAddress}/shopify/callback`;
+        /**
+         * @type {string}
+         * Needs the following
+         *     → Path Variable
+         *          ↳ shop
+         *     → Query Parameters
+         *          ↳ client_id
+         *          ↳ scope
+         *          ↳ state
+         *          ↳ redirectUri
+         */
+        const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&state=${state}&redirect_uri=${redirectUri}`;
+
+        /**
+         * Should be encrypted in real world
+         */
+        res.cookie('state', state);
+        res.redirect(installUrl);
+    } else {
+        return res.status(400)
+            .send('Missing shop parameter. Please add ?shop=your-shop to your request.');
+    }
 });
 
 app.listen(3000, () => {
